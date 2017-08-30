319062b29f1b4c4de50fe7a07561710e4715f30e
anjali0626 <a9sharma@scu.edu>
2017-08-30 15:39:31 -0700

    Create API of Stack implementation using string

---
+++ stack/stackUsingString.js
@@ -0,0 +1,23 @@
+var Stack = function() {
+  this.storage = '';
+};
+
+// Input a value in stack
+Stack.prototype.push = function(val) {
+  // TODO..
+};
+
+// Remove last value from Stack and return the deleted value
+Stack.prototype.pop = function() {
+  // TODO..
+};
+
+// Find length of stack
+Stack.prototype.size = function() {
+  // TODO..
+};
+
+// Display stack
+Stack.prototype.showStack = function(first_argument) {
+  // TODO..
+};
