6230197dae71df72a37e1719de120ecaae334d65
anjali0626 <a9sharma@scu.edu>
2017-08-30 19:50:30 -0700

    Create API of Queue implementation

---
+++ queue/queue.js
@@ -0,0 +1,25 @@
+// Implement Stack data structure using object as storage
+
+var Queue = function() {
+  this.storage = {};
+};
+
+// Input a value in queue
+Queue.prototype.enqueue = function(val) {
+  // TODO...
+};
+
+// Remove first value from queue and return the deleted value
+Queue.prototype.dequeue = function() {
+  // TODO...
+};
+
+// Find length of queue
+Queue.prototype.size = function() {
+  // TODO...
+};
+
+// Display queue
+Queue.prototype.showQueue = function() {
+  // TODO...
+};
