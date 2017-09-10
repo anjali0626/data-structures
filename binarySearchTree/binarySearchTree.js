a129d390f3498bfc840ef95a4be176b9c0f2e11d
anjali0626 <a9sharma@scu.edu>
2017-09-09 18:51:37 -0700

    Create API of Binary Search Tree implementation

---
+++ binarySearchTree/binarySearchTree.js
@@ -0,0 +1,30 @@
+// Implement Binary Search Tree data structure
+
+var BinarySearchTree = function(val) {
+  // TODO...
+};
+
+// Insert a value in it's correct place in binary search tree
+BinarySearchTree.prototype.insert = function(val) {
+  // TODO...
+};
+
+// Find if value is present in binary search tree
+BinarySearchTree.prototype.contains = function(val) {
+  // TODO...
+};
+
+// Traverse binary search tree in depth-first in-order fashion and invoke callback for every node
+BinarySearchTree.prototype.traverseDepthFirst_inOrder = function(callback) {
+  // TODO...
+};
+
+// Traverse binary search tree in depth-first pre-order fashion and invoke callback for every node
+BinarySearchTree.prototype.traverseDepthFirst_preOrder = function(callback) {
+  // TODO...
+};
+
+// Traverse binary search tree in depth-first post-order fashion and invoke callback for every node
+BinarySearchTree.prototype.traverseDepthFirst_postOrder = function(callback) {
+  // TODO...
+};
