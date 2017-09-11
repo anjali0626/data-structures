f1851c40dacf3da3ac82d6544b5a8783fefd0b46
anjali0626 <a9sharma@scu.edu>
2017-09-11 15:19:16 -0700

    Create API of Hash Table implementation

---
+++ hashTable/hashTable.js
@@ -0,0 +1,35 @@
+// Implement Hash Table data structure
+
+var HashTable = function() {
+  // TODO...
+};
+
+// Store the passed in key-value pair in hash table
+HashTable.prototype.set = function(key, value) {
+  // TODO...
+};
+
+// Return the value associated with passed in key in hash table
+HashTable.prototype.get = function(key) {
+  // TODO...
+};
+
+// Delete value associated with the passed in key in hash table
+HashTable.prototype.delete = function(key) {
+  // TODO...
+};
+
+// Find if a value is associated with the passed in key in hash table
+HashTable.prototype.has = function(key) {
+  // TODO...
+};
+
+// Invoke passed in callback for each key-value pair in hash table
+HashTable.prototype.forEach = function(callback) {
+  // TODO...
+};
+
+// Find total number of key-value pairs in hash table
+HashTable.prototype.count = function() {
+  // TODO...
+};
